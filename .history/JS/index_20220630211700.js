1e3beb53e72b43c5410113dd50dc0fbaeb8296ce
herjuandlonely <1026275854@qq.com>
2022-06-30 21:18:36 +0800

    myBlog v1.51

--- .history/JS/index_20220630211700.js
+++ .history/JS/index_20220630211700.js
@@ -97,7 +97,7 @@
                     isHide.classList.add('showAndHide');
                     foldBtn.classList.remove('show');
                 };
-                if (banner.scrollTop > 0) {
+                if (isHide.scrollTop > 0) {
                     isHide.classList.remove('showAndHide');
                 }
             }
